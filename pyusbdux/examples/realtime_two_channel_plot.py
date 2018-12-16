b32af7736982b66d2180cc11475d346cd53c56ef
Lauren Lusk <laurenlusk77@yahoo.com>
2018-12-16 16:33:47 +0000

    Update realtime_two_channel_plot.py

--- pyusbdux/examples/realtime_two_channel_plot.py
+++ pyusbdux/examples/realtime_two_channel_plot.py
@@ -18,13 +18,24 @@ import pyusbdux as c
 import scipy.signal as sig
 
 class IIR2Filter:
-    scale = 2**10
-    fs = 250
+    """
+    Implements a 2nd order IIR filter."
+    """
+    scale = 2**10   # scale for the filter coefficients
+    fs = 250        # sampling frequency of the incoming data
     
     def __init__(self,num,sos):
+        """
+        Initializes class.
+        """
         self.dFormI(num,sos)
 
     def dFormI(self,num,sos):
+        """
+        Creates a Direct Form I from sos coefficients,
+        where num is the index of the array with the coefficients.
+        """
+        #gets coefficients 
         sos = sos[num]
         self.b0 = sos[0]
         self.b1 = sos[1]
@@ -32,17 +43,20 @@ class IIR2Filter:
         self.a1 = sos[4]
         self.a2 = sos[5]
         
+        # initializes buffer to zero
         self.x1 = 0
         self.x2 = 0
         self.y1 = 0
         self.y2 = 0
         
     def filter(self,x):
-        
+        """
+        Filters the data in real time.
+        """
         out = x*self.b0 + self.x1*self.b1 + self.x2*self.b2 - self.y1*self.a1 - self.y2*self.a2
+        out = np.round(out/self.scale)  #removes scale factor
         
-        out = np.round(out/self.scale)
-        
+        # updates buffer
         self.x2 = self.x1
         self.y2 = self.y1
         self.x1 = x
@@ -52,17 +66,29 @@ class IIR2Filter:
     
 
 class IIRFilter:
-    scale = 2**10
+    """
+    This class creates a chain of 2nd order filter instances of IIR2Filter.
+    """
+    scale = 2**10   # scale for the filter coefficients
     
     def __init__(self,sos):
+        """
+        Initializes instances.
+        """
         self.getInstances(sos)
     
     def getInstances(self,sos):
-        sos = np.round(sos*self.scale)
+        """
+        Creates IIR2Filter objects.
+        """
+        sos = np.round(sos*self.scale)  #scales the coefficients
         self.first = IIR2Filter(0,sos)
         self.second = IIR2Filter(1,sos)
     
     def filter(self,x):
+        """
+        Filters data in real time.
+        """
         x2 = self.first.filter(x)
         y = self.second.filter(x2)
         
@@ -104,36 +130,46 @@ class QtPanningPlot:
 
 
 def getDataThread(qtPanningPlot1,qtPanningPlot2):
-    # create stopband filter for 50Hz
-    scale = 2**11
+    """
+    Takes unfiltered data from input and filters out the noise.
+    """
+    # creates stopband filter for 50Hz
+    scale = 2**11   # scale for input (raw data between -1 and 1)
     fs = 250
     fc = 50
     sos = sig.butter(2,[(fc-5)/fs*2,(fc+5)/fs*2],'stop',output='sos')
     filt = IIRFilter(sos)
-    global data
-    #detect = []
-    i=0
+   
+    global data # data collected for future figures
+    # i and last used to create a time buffer
+    # used to reduce the number of times each word is printed
+    # and reduce the influence of the wire noise
+    i=0      
     last = 0
     # endless loop with sleeps not for timing but for multitasking
+    # collects data from input
     while running:
         # loop as fast as we can to empty the kernel buffer
         while c.hasSampleAvailable():
             sample = c.getSampleFromBuffer()
-            sample = sample[channel]*scale
+            sample = sample[channel]*scale  # scales input
+            # filters out 50Hz
             sample2 = filt.filter(int(np.round(sample)))
+            # adds samples to real time graphs
             qtPanningPlot1.addData(sample)
             qtPanningPlot2.addData(sample2)
+            # saves unfiltered data for figures later
             data.append(sample)
-            
          
-            #detect.append(sample2)
+            # detects whether the object was pushed or pulled
             if sample2 >= 200 and i>=last:
                 print('PULL')
-                last = i+200  
+                last = i+200
             elif sample2 <= -100 and i>=last:
                 print('PUSH')
-                last = i+150        
-            i+=1
+                last = i+150 
+            else:
+                i+=1
             
         # let Python do other stuff and sleep a bit
         sleep(0.1)
